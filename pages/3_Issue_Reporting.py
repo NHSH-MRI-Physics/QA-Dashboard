0d73d08383c7ade71d9e7b6b59c99e5b4bb20b4d
John Tracey <Johnt717@gmail.com>
2024-11-07 16:12:14 +0000

    Update 3_Issue_Reporting.py

--- pages/3_Issue_Reporting.py
+++ pages/3_Issue_Reporting.py
@@ -13,4 +13,6 @@ If any scanner issues are noted please report them here.
 
 ReporterName = st.text_input("Reporters Name")
 Issue = st.text_area("Enter issue text, ⚠️ NO PATIENT INFORMATION MUST BE ENTERED HERE! ⚠️")
-st.button("Submit", type="primary")
+
+if st.button("Submit", type="primary"):
+    pass
