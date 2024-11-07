72cf4371401e76297b2562c49520f5520e14b705
John Tracey <Johnt717@gmail.com>
2024-11-07 14:48:24 +0000

    Getting Started

---
+++ Home_Page.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="NHSH MRI Dashboard",
+    page_icon="🏥",
+)
+
+st.write("# NHS Highland MRI Dashboard")
+
+st.markdown(
+"""
+This is the NHS Highland MRI Dashboard, this contains information on the various QA ran as well as results from said QA. 
+
+Select a component from the side menu. 
+### MRI Staff
+- John Tracey - John.tracey@nhs.scot
+- Jonathan Ashmore - TO BE ADDED
+""")
