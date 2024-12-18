11ed6a2d38e2671c69bb112111d7eb56c0b7c221
DrSpacemanMD <Johnt717@gmail.com>
2024-12-18 17:28:24 +0000

    Working on the distortion

--- Home_Page.py
+++ Home_Page.py
@@ -13,9 +13,7 @@ This is the NHS Highland MRI Dashboard, this contains information on the various
 
 Select a component from the side menu. 
 
-If an scanner issue is required to be reported, please report it via the issue reporting page. 
-
 ### MRI Staff
 - John Tracey - John.tracey@nhs.scot
-- Jonathan Ashmore - TO BE ADDED
+- Jonathan Ashmore - Jonathan.ashmore@nhs.scot
 """)
