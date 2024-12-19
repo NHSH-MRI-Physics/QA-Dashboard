ce853e8ca8d666c5cc83106ee7b661eff1b9840d
DrSpacemanMD <Johnt717@gmail.com>
2024-12-19 18:18:55 +0000

    QA Dashboard work

--- Home_Page.py
+++ Home_Page.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import pages.Helper as Helper
 
 st.set_page_config(
     page_title="NHSH MRI Dashboard",
     page_icon="🏥",
 )
 
-st.write("# NHS Highland MRI Dashboard")
+st.header(" NHS Highland MRI Dashboard")
 
 st.markdown(
 """
@@ -16,4 +17,59 @@ Select a component from the side menu.
 ### MRI Staff
 - John Tracey - John.tracey@nhs.scot
 - Jonathan Ashmore - Jonathan.ashmore@nhs.scot
+""") 
+
+st.subheader("Current QA Status")
+
+#DQA
+sh = Helper.GetSheet()
+DQAdates = sh.worksheet("DailyQA").col_values(1)
+DQAQAType = sh.worksheet("DailyQA").col_values(2)
+DQAResults = sh.worksheet("DailyQA").col_values(3)
+DQAScanner = sh.worksheet("DailyQA").col_values(4)
+
+DQAMRI1Index = None
+DQAMRI2Index = None
+for i, e in reversed(list(enumerate(DQAdates))):
+    if DQAScanner[i]=="MRI 1" and DQAMRI1Index==None:
+        DQAMRI1Index=i
+    if DQAScanner[i] == "MRI 2" and DQAMRI2Index==None:
+        DQAMRI2Index=i
+    
+    if DQAMRI1Index!= None and DQAMRI2Index!= None:
+        break
+
+#Distortion
+Distortdates = sh.worksheet("DistortionQA").col_values(1)
+DistortScanner = sh.worksheet("DistortionQA").col_values(2)
+DistortInterplate = sh.worksheet("DistortionQA").col_values(3)
+DistortIntraplate = sh.worksheet("DistortionQA").col_values(4)
+
+
+DistortMRI1Index = None
+DistortMRI2Index = None
+
+for i, e in reversed(list(enumerate(Distortdates))):
+    if DistortScanner[i]=="Raigmore Hospital MRI 1" and DistortMRI1Index==None:
+        DistortMRI1Index=i
+    if DistortScanner[i] == "Raigmore Hospital MRI 2" and DistortMRI2Index==None:
+        DistortMRI2Index=i
+    
+    if DistortMRI1Index!= None and DistortMRI2Index!= None:
+        break
+
+ResultMRI1 = ":red[Fail]"
+if DQAResults[DQAMRI1Index] == "Pass":
+    ResultMRI1 = ":green[Pass]"
+ResultMRI2 = ":red[Fail]"
+if DQAResults[DQAMRI2Index] == "Pass":
+    ResultMRI2 = ":green[Pass]"
+st.markdown(
+f"""
+- MRI 1
+    * DailyQA
+        * Date & Time: {DQAdates[DQAMRI1Index]}
+        * QA Type:     {DQAQAType[DQAMRI1Index]}
+        * Result:      {ResultMRI1}
+- MRI 2
 """) 
---
+++ pages/Helper.py
@@ -0,0 +1,20 @@
+import gspread
+import streamlit as st
+
+
+def GetSheet():
+    credentials = {
+        "project_id": st.secrets["project_id"],
+        "private_key_id": st.secrets["project_id"],
+        "private_key": st.secrets["private_key"],
+        "client_email": st.secrets["client_email"],
+        "client_id": st.secrets["client_id"],
+        "auth_uri": st.secrets["auth_uri"],
+        "token_uri": st.secrets["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+        "universe_domain": st.secrets["universe_domain"]
+    }
+    gc = gspread.service_account_from_dict(credentials)
+    sh = gc.open("QA Record")
+    return sh
